ffc3782c77b8523e681094256b269e3f543decf9
Tommy Persson <tommy.persson@gmail.com>
2021-03-06 07:46:36 +0100

    Add "Jump to Source" action to story tree nodes

--- src/main/java/se/fortnox/intellij/jbehave/ui/storyexplorer/nodes/StoryNodeUserData.kt
+++ src/main/java/se/fortnox/intellij/jbehave/ui/storyexplorer/nodes/StoryNodeUserData.kt
@@ -1,16 +1,25 @@
 package se.fortnox.intellij.jbehave.ui.storyexplorer.nodes
 
 import com.github.kumaraman21.intellijbehave.language.JBehaveIcons
+import com.intellij.icons.AllIcons
+import com.intellij.openapi.ui.JBMenuItem
+import com.intellij.openapi.ui.JBPopupMenu
 import com.intellij.psi.PsiFile
 import com.intellij.ui.ColoredTreeCellRenderer
 import com.intellij.ui.SimpleTextAttributes
 import se.fortnox.intellij.jbehave.ui.storyexplorer.directoryPathRelativeToSourceRoot
 import se.fortnox.intellij.jbehave.ui.storyexplorer.pathAsPackage
+import java.awt.event.ActionEvent
+import javax.swing.AbstractAction
 import javax.swing.tree.DefaultMutableTreeNode
 
 class StoryNodeUserData private constructor(
     private var _file: PsiFile
 ) : StoryTreeNodeUserData {
+
+    private val _popupMenu by lazy { createPopupMenu() }
+
+    val popupMenu: JBPopupMenu get() = _popupMenu
 
     fun update(file: PsiFile): Boolean {
         if (_file == file) {
@@ -23,6 +32,10 @@ class StoryNodeUserData private constructor(
 
     fun wrapInTreeNode() = DefaultMutableTreeNode(this)
 
+    fun jumpToSource() {
+        _file.navigate(true)
+    }
+
     override fun render(renderer: ColoredTreeCellRenderer): Unit = with(renderer) {
         icon = JBehaveIcons.JB
 
@@ -30,6 +43,19 @@ class StoryNodeUserData private constructor(
 
         append("$pkg/")
         append(_file.name, SimpleTextAttributes.REGULAR_BOLD_ATTRIBUTES)
+    }
+
+    private fun createPopupMenu(): JBPopupMenu {
+        val navigateToScenarioActionItem = JBMenuItem(object : AbstractAction() {
+            override fun actionPerformed(e: ActionEvent) = jumpToSource()
+        }).also {
+            it.text = "Jump to Source"
+            it.icon = AllIcons.Actions.EditSource
+        }
+
+        return JBPopupMenu().also {
+            it.add(navigateToScenarioActionItem)
+        }
     }
 
     companion object {
--- src/main/java/se/fortnox/intellij/jbehave/ui/storyexplorer/tree/StoryTreeSelectionListener.kt
+++ src/main/java/se/fortnox/intellij/jbehave/ui/storyexplorer/tree/StoryTreeSelectionListener.kt
@@ -3,6 +3,7 @@ package se.fortnox.intellij.jbehave.ui.storyexplorer.tree
 import com.intellij.ui.treeStructure.Tree
 import com.intellij.util.castSafelyTo
 import se.fortnox.intellij.jbehave.ui.storyexplorer.nodes.ScenarioNodeUserData
+import se.fortnox.intellij.jbehave.ui.storyexplorer.nodes.StoryNodeUserData
 import javax.swing.event.TreeSelectionEvent
 import javax.swing.event.TreeSelectionListener
 import javax.swing.tree.DefaultMutableTreeNode
@@ -11,11 +12,13 @@ class StoryTreeSelectionListener(
     private val storyTree: Tree
 ) : TreeSelectionListener {
     override fun valueChanged(e: TreeSelectionEvent) {
-        val selectedUserData = e.newLeadSelectionPath.lastPathComponent.castSafelyTo<DefaultMutableTreeNode>()?.userObject
-        if (selectedUserData is ScenarioNodeUserData) {
-            storyTree.componentPopupMenu = selectedUserData.popupMenu
-        } else {
-            storyTree.componentPopupMenu = null
+        val selectedUserData = e.newLeadSelectionPath.lastPathComponent.castSafelyTo<DefaultMutableTreeNode>()
+            ?.userObject
+
+        storyTree.componentPopupMenu = when (selectedUserData) {
+            is ScenarioNodeUserData -> selectedUserData.popupMenu
+            is StoryNodeUserData -> selectedUserData.popupMenu
+            else -> null
         }
     }
 }
